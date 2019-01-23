901986a9b5d03a74a44c4e59faa059f3a2a92222
刘宏庆-rubydev <liuhongqing@sudiyi.cn>
2019-01-23 20:22:52 +0800

    user add rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'home/index'
+  namespace :api, default: { format: :json } do
+    scope module: :v1 do
+
+    end
+  end
+  # Ex:- :default =>''
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
